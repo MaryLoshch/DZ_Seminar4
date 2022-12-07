7d6c2d3f443e2083912a066ddffe67f36e61b2d0
мария <oops.mari@mail.ru>
2022-12-07 16:46:45 +0300

    Задача 25

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+// double result = Math.Pow(numA, numB);
+// Console.WriteLine($"Число A в степени B = {result}");
+
+int Stepen(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++) result = result * numA;
+    return result;
+}
+Console.WriteLine($"Число {numA} в степени {numB} равно {Stepen(numA,numB)}");
