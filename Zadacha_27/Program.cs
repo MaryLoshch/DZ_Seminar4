9ca26dd8e2ee6360e9e7df58a883f0e0fa4fa510
мария <oops.mari@mail.ru>
2022-12-07 17:29:26 +0300

    Задача 27

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        int i = num % 10;
+        num = num / 10;
+        sum = sum + i;
+    }
+    return sum;
+}
+Console.WriteLine($"Ответ: {SumNumber(num)}");
