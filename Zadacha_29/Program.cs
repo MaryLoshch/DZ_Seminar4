d96ee7b874f48d4fef67448560c64f3e9045dc0b
мария <oops.mari@mail.ru>
2022-12-07 20:47:53 +0300

    Задача 29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,27 @@
+﻿
+static void GetArray()
+{
+    int[] ArrayNum = new int[8];
+
+
+    for (int i = 0; i < ArrayNum.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент массива под индексом {i}: ");
+        ArrayNum[i] = int.Parse(Console.ReadLine());
+
+    }
+
+
+
+
+    Console.WriteLine("Получили массив: ");
+
+    for (int i = 1; i < ArrayNum.Length; i++)
+    {
+        Console.Write(ArrayNum[i] + " ");
+    }
+
+
+    Console.ReadKey();
+}
+GetArray();
